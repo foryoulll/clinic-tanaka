9872e1d371f1b700b76c23758b8612fab2e76df5
foryoulll <63904761+foryoulll@users.noreply.github.com>
2021-02-02 15:13:47 +0900

    Create setting.js

---
+++ js/setting.js
@@ -0,0 +1,12 @@
+
+
+$('.slick-box.type_img.arrows').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	prevArrow:'<div class="prev"></div>',
+	nextArrow:'<div class="next"></div>',
+	autoplay: true,
+	autoplaySpeed: 6000
+});
